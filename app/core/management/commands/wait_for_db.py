acd5ef1093280006a27ac6ce026c4607132d8bee
Sumedh Waghmare <sumedhw71@gmail.com>
2023-05-28 18:29:35 +0530

    bug fixes

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -14,9 +14,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Entrypoint for command."""
-        self.stdout.write('Waiting for database')
+        self.stdout.write('Waiting for database...')
         db_up = False
-        while db_up:
+        while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
